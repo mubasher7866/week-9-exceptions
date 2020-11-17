eeb78327af12de0294dd019704414f2cc6b6ed65
Mubasher <21694@student.dorset-college.ie>
2020-11-17 13:03:03 +0000

    first commit

--- src/com/company/DivideByZeroException.java
+++ src/com/company/DivideByZeroException.java
@@ -1,16 +1,17 @@
 package com.company;
+import java.util.Scanner;
 
 public class DivideByZeroException {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner (Systeim.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter an integer numerator\t");
         int numerator = scanner.nextInt();
         System.out.print("Please enter an integer denominator\t");
         int denominator = scanner.nextInt();
         int result = quotient(numerator, denominator);
-        System.out.print("%nResult: %d / %d = %d%n", numerator, denominator, result);
+        System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
 
     }
         public static int quotient(int numerator, int denominator){
